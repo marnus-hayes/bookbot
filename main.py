117c6282c9c1f25c864d4e8512837328ba2d50a6
marnus-hayes <marnushayes@outlook.com>
2024-05-07 21:27:30 +0200

    add letter counting

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 def main():
     filepath = "books/frankenstein.txt"
+
     file_contents = get_book_text(filepath)
     word_count = count_words(file_contents)
+    letter_count = count_letters(file_contents)
+
     print(f"{word_count} words found in book!")
+    print("Here are the amount of times different letters appeared in the book:")
+    print(letter_count)
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -11,5 +16,21 @@ def get_book_text(filepath):
 def count_words(text):
     word_list = text.split()
     return len(word_list)
+
+def count_letters(text):
+    alphabet = list("abcdefghijklmnopqrstuvwxyz")
+    lowercase_text = text.lower()
+    letters = list(lowercase_text)
+    counts = {}
+
+    for letter in letters:
+        if letter in alphabet:
+            if letter in counts:
+                counts[letter] += 1
+            else:
+                counts[letter] = 0
+    return counts
+
+
 
 main()
